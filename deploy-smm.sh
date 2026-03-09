27d6d0f2e88d7dad3f23227ec2736b294a674a87
Ersan <ersanjahedtabrizi@gmail.com>
2026-03-09 22:28:07 +0300

    Deploy: exclude config.php, fix permissions (chmod 755/644)

--- deploy-smm.sh
+++ deploy-smm.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 cd /home/smmturk/repositories/smm-turk-panel
 git pull origin main
-rsync -av --delete --exclude=".git" --exclude=".cpanel.yml" ./ /home/smmturk/public_html/
+rsync -av --delete --exclude=".git" --exclude=".cpanel.yml" --exclude="config.php" --chmod=D755,F644 ./ /home/smmturk/public_html/
